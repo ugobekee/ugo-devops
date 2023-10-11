e0b0279f2fe9de1b99edcc8dd98d1100d2cb4cf6
ugobekee <99548862+ugobekee@users.noreply.github.com>
2023-10-11 19:55:46 -0400

    Create UHM8G05_CDNA343.sql

---
+++ UHM8G05_CDNA343.sql
@@ -0,0 +1,10 @@
+def createTableSQL = """
+                        CREATE TABLE CUST_CDNA_STG.CUST_DNA.UHM8G05_CDNA343 (
+                            ekey string,
+                            email1 string,
+                            email2 string,
+                            email3 string,
+                            phone1 string,
+                            phone2 string,
+                            phone3 string,
+                        );
